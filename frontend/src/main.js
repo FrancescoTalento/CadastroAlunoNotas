e7b9e132e68e64ba5d8ada254ed8593863d34e7d
Snou <francescotalento281@gmail.com>
2025-04-30 10:53:07 -0300

    Making the 'Alunos' visible to the user

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,46 @@
-import './style.css'
-import api from './api.js'
-import ux from './ux.js'
+import "./style.css";
+import api from "./api.js";
+import ux from "./ux.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+  carregaUx();
+
+  const form = document.querySelector("#formularioAluno");
+  form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    await criarAlunoJson();
+    carregaUx();
+    form.reset();
+  });
+});
+
+function carregaUx() {
+  ux.mostraAlunos();
+  ux.mostraAlunosComFrequenciaBaixa();
+  ux.mostraAlunosAcimaDaMedia();
+  ux.mostraMediaTurmaPorDisciplina();
+}
+async function criarAlunoJson() {
+  const nomeAluno = document.querySelector("#nome").value;
+
+  const notas = [
+    "Matemática",
+    "Português",
+    "História",
+    "Geografia",
+    "Ciências",
+  ].map((disciplina, i) => ({
+    disciplina,
+    valor: document.querySelector(`#nota${i + 1}`).value,
+  }));
+
+  const frequencia = document.querySelector("#frequencia").value;
+
+  const aluno = {
+    nome: nomeAluno,
+    frequencia: frequencia,
+    notas,
+  };
+
+  await api.cadastraAluno(aluno);
+}
--- frontend/src/ux.js
+++ frontend/src/ux.js
@@ -1,7 +1,89 @@
 import api from "./api.js";
+const ux = {
+    
+  async mostraMediaTurmaPorDisciplina() {
+    try {
+      const mediaNotas = await api.buscaMediaNotasTurma();
+      const listaMediaNotas = document.querySelector("#mediaPorDisciplina");
+        
+    
+      listaMediaNotas.innerHTML = "";
 
-const ux{
-    show
-}
+      mediaNotas.forEach((item) => {
+        const li = document.createElement("li");
+        li.innerHTML = `<strong>${item.disciplina}:</strong> ${item.media}`;
+        listaMediaNotas.appendChild(li);
+      });
+    } catch (error) {
+      console.error("Erro ao buscar médias por disciplina:", error);
+    }
+  },
+  async mostraAlunosAcimaDaMedia() {
+    try {
+      const alunosJson = await api.buscaAlunosAcimaDaMedia();
+      const listaAlunos = document.querySelector("#mediaAlta");
+
+      listaAlunos.innerHTML = "";
+      alunosJson.forEach((aluno) => {
+        listaAlunos.appendChild(this.criaItemAluno(aluno));
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  async mostraAlunosComFrequenciaBaixa() {
+    try {
+      const alunosJson = await api.buscaAlunosEmAtencao();
+      const listaAlunos = document.querySelector("#frequenciaBaixa");
+
+      listaAlunos.innerHTML = "";
+      alunosJson.forEach((aluno) => {
+        listaAlunos.appendChild(this.criaItemAluno(aluno));
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  criaItemAluno(aluno) {
+    const liAluno = document.createElement("li");
+    liAluno.textContent = aluno.nome;
+
+    return liAluno;
+  },
+  async mostraAlunos() {
+    try {
+      const tBody = document.querySelector("#tabelaAluno");
+      const alunosJson = await api.buscaAlunos();
+
+      tBody.innerHTML = ""
+      alunosJson.forEach((aluno) => {
+        tBody.appendChild(this.criaTrAluno(aluno));
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  criaTrAluno(aluno) {
+    const tr = document.createElement("tr");
+    const tdNome = document.createElement("td");
+    const tdMediaD = document.createElement("td");
+    const tdFrequencia = document.createElement("td");
+
+    tdNome.textContent = aluno.nome;
+    tdFrequencia.textContent = aluno.frequencia + "%";
+
+    // Juntar todas as médias por disciplina com quebra de linha
+    tdMediaD.innerHTML = aluno.mediasPorDisciplina
+      .map((d) => `<strong>${d.disciplina}:</strong> ${d.media}`)
+      .join("<br>");
+
+    tr.appendChild(tdNome);
+    tr.appendChild(tdMediaD);
+    tr.appendChild(tdFrequencia);
+
+    return tr;
+  },
+};
 
 export default ux;
