32efeb1b3d29d72eccbe9e550e2dddb6895d526a
Snou <francescotalento281@gmail.com>
2025-04-30 18:41:49 -0300

    Added the DELETE and PUT method in the front-end 'API'

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,3 @@
-
-
 const BASE_URL = "http://localhost:5251/api/alunos";
 
 // Funcao generica de fetch com tratamento de erro
@@ -13,6 +11,17 @@ async function fetchJson(url, options = {}, errorMsg = "Erro na requisição") {
     throw error;
   }
 }
+
+async function fetchVoid(url, options = {}, errorMsg = "Erro na requisição") {
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+  } catch (error) {
+    console.error(`${errorMsg}:`, error);
+    throw error;
+  }
+}
+
 
 const api = {
   buscaAlunos() {
@@ -59,7 +68,40 @@ const api = {
       return dados;
     } catch (error) {
       alert("Erro ao adicionar aluno.");
-      console.log(error)
+      console.log(error);
+    }
+  },
+  async atualizaAluno(id, aluno) {
+    try {
+      await fetchVoid(
+        `${BASE_URL}/${id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(aluno),
+        },
+        "Erro ao atualizar aluno"
+      );
+      alert("Aluno atualizado com sucesso!");
+    } catch (error) {
+      alert("Erro ao atualizar aluno.");
+      console.log(error);
+    }
+  },
+
+  async deletaAluno(id) {
+    try {
+      await fetchVoid(
+        `${BASE_URL}/${id}`,
+        {
+          method: "DELETE",
+        },
+        "Erro ao deletar aluno"
+      );
+      alert("Aluno deletado com sucesso!");
+    } catch (error) {
+      alert("Erro ao deletar aluno.");
+      console.log(error);
     }
   },
 };
