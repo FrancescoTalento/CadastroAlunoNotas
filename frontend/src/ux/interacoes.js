8d83cf06f0b32329f1e27455464c7ff9acba63de
Snou <francescotalento281@gmail.com>
2025-04-30 20:17:39 -0300

    Feature: centralized control option to-do on each student

---
+++ frontend/src/ux/interacoes.js
@@ -0,0 +1,52 @@
+import api from "../api.js";
+import ux from "./exibicao.js";
+import {
+  handleSubmitEdicao,
+  preencherFormularioDeEdicao,
+  handlerCancelEdicao,
+} from "../Helpers/edicaoFormHelper.js";
+
+import {
+  handleSubmitNovaNota,
+  handleResetNovaNota,
+} from "../Helpers/novaNotaFormHelper.js";
+
+const interacoesUx = {
+  async removerAluno(id) {
+    await api.deletaAluno(id);
+    await ux.mostraTodosAlunos();
+  },
+  async editaAluno(id, aluno) {
+    preencherFormularioDeEdicao(aluno);
+
+    const form = document.querySelector("#formularioEdicao");
+    const novoForm = form.cloneNode(true); 
+    
+    form.replaceWith(novoForm);
+
+    novoForm.addEventListener("submit", handleSubmitEdicao);
+    novoForm.addEventListener("reset", handlerCancelEdicao);
+  },
+
+  async adicionarNota(id, aluno) {
+    const section = document.querySelector("#adicionarNotas");
+    const form = document.querySelector("#formularioNotas");
+    const nome = document.querySelector("#nova-notaNome");
+
+    section.classList.remove("hidden");
+
+    nome.textContent = `Adicionando Nota para ${aluno.nome}`;
+
+    const novoForm = form.cloneNode(true);
+    form.replaceWith(novoForm);
+
+    novoForm.addEventListener(
+      "submit",
+      async (event) => await handleSubmitNovaNota(event, id)
+    );
+
+    novoForm.addEventListener("reset", (event) => handleResetNovaNota(event));
+  },
+};
+
+export default interacoesUx;
