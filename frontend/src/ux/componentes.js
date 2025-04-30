b3395a0627bb9b6aeb5e00a653bf2166cebb14de
Snou <francescotalento281@gmail.com>
2025-04-30 18:39:28 -0300

    Added and fixed Modularity to the UX logic

--- frontend/src/ux/componentes.js
+++ frontend/src/ux/componentes.js
@@ -1,27 +1,61 @@
+import interacoesUx from "./interacoes.js";
 
-
-export function criaItemAluno(aluno) {
+export function criaItemAluno(aluno,type=null) {
   const liAluno = document.createElement("li");
   liAluno.textContent = aluno.nome;
-  return liAluno;
+  if(type=== null){
+    return liAluno;
+  }else if(type === "frequencia"){
+    liAluno.textContent+= `: ${aluno.frequencia}% de frequencia`
+    return liAluno
+  }else if(type==="media"){
+    console.log(aluno)
+      liAluno.textContent += `: ${aluno.mediaAluno} de media geral`;
+      return liAluno;
+  }
 }
+
+
 
 export function criaTrAluno(aluno) {
   const tr = document.createElement("tr");
+
   const tdNome = document.createElement("td");
-  const tdMediaD = document.createElement("td");
-  const tdFrequencia = document.createElement("td");
-
   tdNome.textContent = aluno.nome;
-  tdFrequencia.textContent = aluno.frequencia + "%";
 
-  tdMediaD.innerHTML = aluno.mediasPorDisciplina
+  const tdMedia = document.createElement("td");
+  tdMedia.innerHTML = aluno.mediasPorDisciplina
     .map((d) => `<strong>${d.disciplina}:</strong> ${d.media}`)
     .join("<br>");
 
-  tr.appendChild(tdNome);
-  tr.appendChild(tdMediaD);
-  tr.appendChild(tdFrequencia);
+  const tdFreq = document.createElement("td");
+  tdFreq.textContent = aluno.frequencia + "%";
 
+  const tdAcoes = document.createElement("td");
+  const btnExcluir = document.createElement("button");
+  btnExcluir.textContent = "Excluir";
+  btnExcluir.classList.add("btn-excluir");
+  btnExcluir.addEventListener("click", () =>
+    interacoesUx.removerAluno(aluno.id)
+  );
+
+  const btnEditar = document.createElement("button");
+  btnEditar.textContent = "Editar";
+  btnEditar.classList.add("btn-editar");
+  btnEditar.addEventListener("click", () =>
+    interacoesUx.editaAluno(aluno.id, aluno)
+  );
+  const btnAddNota = document.createElement("button");
+  btnAddNota.textContent = "AddNota";
+  btnAddNota.classList.add("btn-addNota");
+  btnAddNota.addEventListener("click", () =>
+    interacoesUx.adicionarNota(aluno.id, aluno)
+  );
+
+  tdAcoes.appendChild(btnExcluir);
+  tdAcoes.appendChild(btnEditar);
+  tdAcoes.appendChild(btnAddNota);
+  
+  tr.append(tdNome, tdMedia, tdFreq, tdAcoes);
   return tr;
 }
--- frontend/src/ux/exibicao.js
+++ frontend/src/ux/exibicao.js
@@ -2,6 +2,12 @@ import api from "../api.js";
 import { criaItemAluno, criaTrAluno } from "./componentes.js";
 
 const ux = {
+  async mostraTodosAlunos() {
+    await this.mostraAlunos()
+    await this.mostraAlunosComFrequenciaBaixa();
+    await this.mostraAlunosAcimaDaMedia();
+    await this.mostraMediaTurmaPorDisciplina();
+  },
   async mostraAlunos() {
     const tBody = document.querySelector("#tabelaAluno");
     const alunos = await api.buscaAlunos();
@@ -13,19 +19,27 @@ const ux = {
     const lista = document.querySelector("#frequenciaBaixa");
     const alunos = await api.buscaAlunosEmAtencao();
     lista.innerHTML = "";
-    alunos.forEach((aluno) => lista.appendChild(criaItemAluno(aluno)));
+    alunos.forEach((aluno) =>
+      lista.appendChild(criaItemAluno(aluno, "frequencia"))
+    );
   },
 
   async mostraAlunosAcimaDaMedia() {
     const lista = document.querySelector("#mediaAlta");
     const alunos = await api.buscaAlunosAcimaDaMedia();
     lista.innerHTML = "";
-    alunos.forEach((aluno) => lista.appendChild(criaItemAluno(aluno)));
+    alunos.forEach((aluno) => 
+        lista.appendChild(criaItemAluno(aluno,"media")));
+
   },
 
   async mostraMediaTurmaPorDisciplina() {
+    
     const lista = document.querySelector("#mediaPorDisciplina");
     const medias = await api.buscaMediaNotasTurma();
+    
+
+    
     lista.innerHTML = "";
     medias.forEach((item) => {
       const li = document.createElement("li");
