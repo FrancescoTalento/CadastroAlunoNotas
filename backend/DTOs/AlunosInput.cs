0894b9b5e5879092cfec32c07da2ebcd56819860
Snou <francescotalento281@gmail.com>
2025-05-01 17:25:28 -0300

    Validation on POST method received

--- backend/DTOs/AlunosInput.cs
+++ backend/DTOs/AlunosInput.cs
@@ -1,13 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace backend.DTOs
 {
     public record AlunoInput(
-        string Nome,
-        decimal Frequencia,
-        List<NotaInput> Notas
-    );
+    [Required, StringLength(50, MinimumLength = 3)] string Nome,
+    [Range(0, 100)] decimal Frequencia,
+    [MinLength(1)] List<NotaInput> Notas
+);
 
-    public record NotaInput(
-        int DisciplinaId,
-        decimal Valor
-    );
+public record NotaInput(
+    [Range(1, int.MaxValue)] int DisciplinaId,
+    [Range(0, 10)] decimal Valor
+);
+
 }
