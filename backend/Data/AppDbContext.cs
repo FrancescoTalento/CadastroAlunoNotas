a42abac6c36a28ac57750e1b56074c4d538c4162
Snou <francescotalento281@gmail.com>
2025-04-30 21:53:11 -0300

    Updated project Configurations

--- backend/Data/AppDbContext.cs
+++ backend/Data/AppDbContext.cs
@@ -9,6 +9,16 @@ namespace backend.Data
 
         public DbSet<Estudante> Estudantes => Set<Estudante>();
         public DbSet<Nota> Notas => Set<Nota>();
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Nota>()
+                .HasOne(n => n.Estudante)         
+                .WithMany(e => e.Notas)           
+                .HasForeignKey(n => n.EstudanteId) 
+                .OnDelete(DeleteBehavior.Cascade); 
+        }
     }
 }
 
